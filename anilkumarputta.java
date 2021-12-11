fe1e5d7bdf4d0154cecc565dec061c51e7a7f415
pravalikanil <95961914+pravalikanil@users.noreply.github.com>
2021-12-11 15:59:29 +0530

    Add files via upload

---
+++ anilkumarputta.java
@@ -0,0 +1,11 @@
+class anilkumarputta 
+{
+public static void main(String ar[]) 
+{
+    boolean a=true;
+    boolean b=false;
+    System.out.println("the result is"+(a&b));
+    System.out.println("the result is"+(a|b));
+    System.out.println("the result is"+(!a));
+}   
+}
